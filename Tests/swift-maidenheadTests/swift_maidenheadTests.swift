08c3e5eaef242f7b4d59c47eb9c2aae1f8dc0de5
Tomas Nevrlka <tomas@nevrlka.com>
2023-06-30 21:19:56 +0200

    test: add field calculation tests

--- Tests/swift-maidenheadTests/swift_maidenheadTests.swift
+++ Tests/swift-maidenheadTests/swift_maidenheadTests.swift
@@ -1,12 +1,29 @@
 import XCTest
 @testable import swift_maidenhead
+import CoreLocation
+
+struct TestCase {
+    let name: String
+    let location: CLLocationCoordinate2D
+    let locator: String
+    
+    init(name: String, latitude: CLLocationDegrees, longitude: CLLocationDegrees, locator: String) {
+        self.name = name
+        self.location = CLLocationCoordinate2D.init(latitude: latitude, longitude: longitude)
+        self.locator = locator
+    }
+}
+
+let testCases = [TestCase(name: "W1AW", latitude: 41.71463, longitude: -72.72713, locator: "FN31pr"),
+                 TestCase(name: "Brno", latitude: 49.1951, longitude: 16.6068, locator: "JN89he"),
+                 TestCase(name: "Cape Town", latitude: -33.9249, longitude: 18.4241, locator: "JF96fb")
+]
 
 final class swift_maidenheadTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testFieldCalculation() {
+        for testCase in testCases {
+            let maidenhead = Maidenhead(precision: Precision.field, location: testCase.location)
+            XCTAssert(maidenhead.locator == testCase.locator.prefix(2))
+        }
     }
 }
